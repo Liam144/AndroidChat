d5370e75e7875c4408dd983af78414f744d0dda6
liam144 <willfersuar@hotmail.com>
2016-06-14 00:12:08 -0500

    Creamos actividad Screen e implementamos la Vista

--- app/src/main/java/edu/udistrital/android/androidchat/login/LoginActivity.java
+++ app/src/main/java/edu/udistrital/android/androidchat/login/LoginActivity.java
@@ -1,8 +1,12 @@
 package edu.udistrital.android.androidchat.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -11,19 +15,24 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import edu.udistrital.android.androidchat.R;
+import edu.udistrital.android.androidchat.contactlist.ContactListActivity;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity implements LoginView {
 
     @BindView(R.id.editTxtEmail)
-    EditText editTxtEmail;
+    EditText inputEmail;
     @BindView(R.id.editTxtPassword)
-    EditText editTxtPassword;
+    EditText inputPassword;
     @BindView(R.id.progressBar)
     ProgressBar progressBar;
     @BindView(R.id.layoutMainContainer)
-    RelativeLayout layoutMainContainer;
+    RelativeLayout container;
+    @BindView(R.id.btnSignin)
+    Button btnSignin;
+    @BindView(R.id.btnSignup)
+    Button btnSignup;
 
+    private LoginPresenter loginPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +43,77 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnSignin)
     public void handleSignin() {
-        Log.e("AndroidChat", editTxtEmail.getText().toString());
+        Log.e("AndroidChat", inputEmail.getText().toString());
     }
 
     @OnClick(R.id.btnSignup)
     public void handleSignup() {
-        Log.e("AndroidChat", editTxtEmail.getText().toString());
+        Log.e("AndroidChat", inputEmail.getText().toString());
 
     }
 
+    @Override
+    public void enableInputs() {
+        setInputs(true);
+    }
+
+    @Override
+    public void disableInputs() {
+        setInputs(false);
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.btnSignup)
+    @Override
+    public void handleSingnUp() {
+        loginPresenter.registerNewUser(inputEmail.getText().toString(),
+                                       inputPassword.getText().toString());
+    }
+
+    @OnClick(R.id.btnSignin)
+    @Override
+    public void handleSignIn() {
+        loginPresenter.validateLogin(inputEmail.getText().toString(),
+                                     inputPassword.getText().toString());
+    }
+
+    @Override
+    public void navigateToMainScreen() {
+        startActivity(new Intent(this, ContactListActivity.class));
+    }
+
+    @Override
+    public void loginError(String error) {
+        inputPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signin), error);
+        inputPassword.setError(msgError);
+    }
+
+    @Override
+    public void newUserSuccess() {
+        Snackbar.make(container, R.string.login_notice_message_signup, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void newUserError(String error) {
+        inputPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signup), error);
+        inputPassword.setError(msgError);
+    }
+
+    private void setInputs(boolean enabled) {
+        inputEmail.setEnabled(enabled);
+        inputPassword.setEnabled(enabled);
+        btnSignin.setEnabled(enabled);
+        btnSignup.setEnabled(enabled);
+    }
 }
